38faa07b5eb18060e545c2f937f413ac5255b585
Tamaz Bagdavadze <tamazbagdavadze@gmail.com>
2017-05-19 17:42:25 +0400

    add state pattern

---
+++ State/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace State
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new Context(new ConcreteStateA());
+            context.Request();
+            context.Request();
+        }
+    }
+
+    class Context
+    {
+        public State State { get; set; }
+
+        public Context(State state)
+        {
+            State = state;
+        }
+
+        public void Request()
+        {
+            State.Handle(this);
+        }
+    }
+
+    abstract class State
+    {
+        public abstract void Handle(Context context);
+    }
+
+    class ConcreteStateA : State
+    {
+        public override void Handle(Context context)
+        {
+            context.State = new ConcreteStateB();
+        }
+    }
+
+    class ConcreteStateB : State
+    {
+        public override void Handle(Context context)
+        {
+            context.State = new ConcreteStateA();
+        }
+    }
+}
