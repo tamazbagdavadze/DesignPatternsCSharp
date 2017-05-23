3c46fead9a8c19ee5207b2cb0038e228843be795
Tamaz Bagdavadze <tamazbagdavadze@gmail.com>
2017-05-23 23:23:59 +0400

    add prototype pattern

---
+++ Prototype/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Prototype prototype = null;
+            Prototype clone = null;
+
+            prototype = new ConcretePrototype1(1);
+            clone = prototype.Clone();
+
+            prototype = new ConcretePrototype2(2);
+            clone = prototype.Clone();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Prototype/Prototype.cs
@@ -0,0 +1,14 @@
+﻿namespace Prototype
+{
+    public abstract class Prototype
+    {
+        public int Id { get; private set; }
+
+        public Prototype(int id)
+        {
+            Id = id;
+        }
+
+        public abstract Prototype Clone();
+    }
+}
